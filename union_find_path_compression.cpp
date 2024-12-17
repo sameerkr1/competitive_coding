3a37d54291da32d50c10b540bf3d31676c820994
Sameer kumar singh <60307463+sameerkr1@users.noreply.github.com>
2024-12-18 01:10:58 +0530

    Update union_find_path_compression.cpp

--- union_find_path_compression.cpp
+++ union_find_path_compression.cpp
@@ -6,56 +6,55 @@ using namespace __gnu_pbds;
 #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 #define ll long long
 #define FASTIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define inf 1e9
+#define inf 1e18
 #define ff first
 #define ss second
 #define lb lower_bound
 #define ub upper_bound
 #define bs binary_search
-#define mod 1000000007
 #define pb push_back
 #define all(s) s.begin(),s.end()
 #define pie 3.14159265358979323846
 #define fr(i,a,b) for(ll i=a;i<b;i++)
 #define frr(i,a,b) for(ll i=b-1;i>=a;i--)
 #define gcd __gcd
-#define con continue
 #define pii pair<ll,ll>
-const ll N=1e5+6;
-ll n,a,b,x;
-ll par[N];
-void make_par(){
-   for(ll i=0;i<N;i++)par[i]=i;
+
+int findParent(int node,vector<int>&parent){
+    if(node==parent[node]){
+        return node;
+    }
+    return parent[node]=findParent(parent[node]);
 }
-ll find_par(ll u){
-   if(par[u]==u)return u;
-   return par[u]=find_par(par[u]);
+
+void unionByRank(int u,int v,vector<int>&parent,vector<int>&ranking){
+    int up=findParent(u);
+    int vp=findParent(v);
+    if(up==vp)return;
+    if(ranking[up]<ranking[vp]){
+        parent[up]=vp;
+    }
+    else if(ranking[up]>ranking[vp]){
+        parent[vp]=up;
+    }
+    else{
+        parent[up]=vp;
+        ranking[vp]++;
+    }
 }
-void union_par(ll a,ll b){
-   a=find_par(a);
-   b=find_par(b);
-   par[a]=b;
-   return;
+
+void disjointSet(int n){
+    vector<int>ranking(n+1,0),parent(n+1);
+    for(int i=1;i<=n;i++)parent[i]=i;
 }
+
 signed main(){
     FASTIO;
-    ll tt=1;
-    //cin>>tt;
+    int tt=1;
+    cin>>tt;
     while(tt--){
-         make_par();
-         ll n,m;cin>>m>>n;
-         for(ll i=0;i<n;i++){
-            cin>>x;
-            if(x==1){
-               cin>>a>>b;
-               a=find_par(a);
-               b=find_par(b);
-               if(a==b)cout<<"1\n";
-               else cout<<"0\n";
-               con;
-            }
-            cin>>a>>b;
-            union_par(a,b);
-         }
+        ll n,k;cin>>n>>k;
+        cout<<abs(n-k)<<"\n";
     }
+
 }
